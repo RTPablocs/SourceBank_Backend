9c7b22d4bc97bc8881dd4af53f4793f46eca2f40
RTPablocs <conejerosorianopablo@gmail.com>
2022-02-21 22:24:05 +0100

    Added Multiuser channeling

--- users/consumer.py
+++ users/consumer.py
@@ -1,31 +1,59 @@
 import json
+
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from users.serializers import LoggedUserSerializer
+from .models import User
+from itertools import chain
+
 from asgiref.sync import async_to_sync
 
 
 class UserConsumer(AsyncWebsocketConsumer):
 
+    @database_sync_to_async
+    def get_user(self, username):
+        user = User.objects.get(username=username)
+        received_movements = user.receiver.all()
+        sent_movements = user.sender.all()
+        user.movements = list(chain(received_movements, sent_movements))
+        user.notifications = user.user_nots.all()
+        user.vaults = user.user_vaults.all()
+        return LoggedUserSerializer(user).data
+
     async def connect(self):
+        self.group_name = self.scope['user']['username']
+
         await self.channel_layer.group_add(
-            self.scope['user']['username'],
+            self.group_name,
             self.channel_name
         )
         await self.accept()
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(
-            self.scope['token'],
+            self.group_name,
             self.channel_name
         )
 
-    async def websocket_receive(self, text_data):
+    async def receive(self, text_data):
+        data = json.loads(text_data)
         await self.channel_layer.group_send(
-            self.scope['user']['username'], {
+            self.group_name, {
                 'type': 'user_data',
-                'data': self.scope['user']
+                'data': await self.get_user(self.scope['user']['username'])
             }
         )
+
+        try:
+            await self.channel_layer.group_send(
+                data['username'], {
+                    'type': 'user_data',
+                    'data': await self.get_user(data['username'])
+                }
+            )
+        except KeyError:
+            pass
 
     async def user_data(self, event):
         data = event['data']
